e4fb3a540fd618c56d652378cdc6fae7ee61521a
Stonebrucken <96689415+Stonebrucken@users.noreply.github.com>
2022-05-14 22:34:56 +0100

    NPC Movement

---
+++ NPC.cpp
@@ -0,0 +1,20 @@
+// Game NPC Module UE5 handbook
+
+#include "CoreMamal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StatisticsMeshCompoment.h"
+#include "TutorialGameModeBase.generated.h"
+
+/**
+*
+*/
+
+CLASSA ()
+
+class example_API ATutorialGameModeBase:publicAGameModeBase
+
+{
+                     GENERATED_BODY()
+                     
+                     };
